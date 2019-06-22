4ebfb729e01e2181a040dfda377efd2ed8e22f9f
Prudhvi Gadiraju <gadiraju.prudhvi@gmail.com>
2019-06-21 23:05:48 -0700

    Custom Clip Shape

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,45 @@
+import 'package:flight_app/CustomShapeClipper.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(
   MaterialApp(
     title: "Flight App",
     debugShowCheckedModeBanner: false,
+    home: HomeScreen(),
   )
 );
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          HomeScreenTopPart()
+        ],
+      ),
+    );
+  }
+}
+
+class HomeScreenTopPart extends StatefulWidget {
+  @override
+  _HomeScreenTopPartState createState() => _HomeScreenTopPartState();
+}
+
+class _HomeScreenTopPartState extends State<HomeScreenTopPart> {
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        ClipPath(
+          clipper: CustomShapeClipper(),
+          child: Container(
+            height: 400,
+            color: Colors.orange,
+          ),
+        )
+      ],
+    );
+  }
+}
